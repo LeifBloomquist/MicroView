ae07a618d1c61d13b7489b82552e8738035c5acd
Leif Bloomquist <github@leifbloomquist.net>
2015-01-04 01:41:54 -0500

    Working great!

--- Arduino/MicroSwinth/MicroSwinth.ino
+++ Arduino/MicroSwinth/MicroSwinth.ino
@@ -21,12 +21,14 @@
 #endif
 
 // Global variables
-boolean horizontal_mirror=false;
-boolean vertical_mirror=false;
-int max_inc=4;
-int min_inc=1;
+boolean horizontal_mirror=true;
+boolean vertical_mirror=true;
 int max_x = uView.getLCDWidth();
 int max_y = uView.getLCDHeight();
+
+int max_inc=4;
+int min_inc=1;
+
 int tail_count=0;
 int tail_length=4;
 ByteBuffer buffer;
@@ -52,7 +54,7 @@ void setup()
 
 
 void loop() 
-{
+{ 
   // Plot the first line
   PlotLine(x0,y0,x1,y1);
   delay(20);
@@ -64,7 +66,7 @@ void loop()
   buffer.putFloat(y1);
   
   // Erase "tail"  
-  if (tail_count >= tail_length)
+  if (tail_count >= (tail_length*TAIL_MULT))
   {
      float temp_x0 = buffer.getFloat();
      float temp_y0 = buffer.getFloat();
@@ -84,11 +86,11 @@ void loop()
   x1 += dx1;
   y1 += dy1;
   
-  // "Bounce"
-  if ((x0 > max_x) || (x0 < 1)) dx0=-dx0;        
-  if ((y0 > max_y) || (y0 < 1)) dy0=-dy0;        
-  if ((x1 > max_x) || (x1 < 1)) dx1=-dx1;        
-  if ((y1 > max_y) || (y1 < 1)) dy1=-dy1;     
+  // "Bounce" and then change+negate delta randomly
+  if ((x0 >= max_x) || (x0 < 1)) { x0 -= dx0; dx0=-sgn(dx0)*randomize(); }
+  if ((y0 >= max_y) || (y0 < 1)) { y0 -= dy0; dy0=-sgn(dy0)*randomize(); }
+  if ((x1 >= max_x) || (x1 < 1)) { x1 -= dx1; dx1=-sgn(dx1)*randomize(); }   
+  if ((y1 >= max_y) || (y1 < 1)) { y1 -= dy1; dy1=-sgn(dy1)*randomize(); }   
 
   // Check serial port
   CheckCommands();
@@ -117,10 +119,11 @@ void PlotLine(float _x0,float _y0,float _x1,float _y1)
   uView.display(); 
 }
 
-// Random value between 0.0 and 1.0
+// Random value between 0.01 and 1.00  (0 would cause point to freeze)
+// Multiplied by random increment factor
 float randomize()
 {
-   return random(1000)/1000.0;
+   return (0.01+random(99)/100.0)*random(min_inc,max_inc);
 }
 
 void randomizeAll()
@@ -144,10 +147,10 @@ void CheckCommands()
      
      switch (c)
      {
-       case ' ': ClearScreen();
+       case ' ': ShowCommands();
+                 ClearScreen();
                  randomizeAll();
-                 ClearBuffer();
-                 ShowCommands();
+                 ClearBuffer();                 
                  break;
                 
        case 'h': 
@@ -160,9 +163,30 @@ void CheckCommands()
        case 'V': ClearScreen();
                  ClearBuffer();
                  vertical_mirror = !vertical_mirror;
-                 break;                    
+                 break;  
+
+       case 'i': 
+       case 'I': switch (max_inc)
+                 {
+                    case 4:  max_inc = 8;  break;
+                    case 8:  max_inc = 16; break;
+                    case 16: max_inc = 32; break;
+                    default: max_inc = 4;  break;
+                 }
+                 break;
+
+       case 'e': 
+       case 'E': if (min_inc == 1)
+                 {
+                   min_inc = 2;
+                 }
+                 else
+                 {
+                   min_inc = 1;
+                 }                 
+                 break;
        
-       default: if ((c >= '0') && (c<='9'))
+       default: if ((c >= '0') && (c <= '9'))
                 {
                   ClearScreen();
                   ClearBuffer();
@@ -188,13 +212,22 @@ void ClearScreen()
 
 void ShowCommands()
 {
-  String s = "MicroSwinth Commands\n\n\r\r"  
+  mySerial.print(
+             "MicroSwinth Commands\n\n\r\r"  
              "i     changes max increment 32>16>8>4\n\r" 
              "e     changes min increment 1<->2\n\r" 
              "h     toggles horizonal symmetry\n\r" 
              "v     toggles vertical symmetry\n\r" 
              "0-9   changes length of tail\n\r" 
-             "space resets display\n\n\r\r";
-             
-  mySerial.print(s);
+             "space resets display\n\n\r\r"
+          );
+  
+  mySerial.flush();
+}
+
+int sgn(float val) 
+{
+ if (val < 0) return -1;
+ if (val == 0) return 0;
+ return 1;
 }
