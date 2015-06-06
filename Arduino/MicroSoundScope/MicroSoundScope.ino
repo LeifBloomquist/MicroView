92204571852526f4ec921602487484e78cc2af8f
Leif Bloomquist <github@leifbloomquist.net>
2015-06-06 10:09:20 -0400

    Fixed up averaging

--- Arduino/MicroSoundScope/MicroSoundScope.ino
+++ Arduino/MicroSoundScope/MicroSoundScope.ino
@@ -4,7 +4,7 @@
 #include <MicroView.h>
 
 #define PIN_ENVELOPE_IN A1
-#define PIN_AUDIO_IN A2
+#define PIN_AUDIO_IN A3
 
 #define ENVELOPE_MAX    350
 #define AUDIO_MAX       1023
@@ -14,6 +14,10 @@
 int max_x = uView.getLCDWidth();
 int max_y = uView.getLCDHeight();
 int mid_y = max_y/2;
+int last_y = mid_y;
+
+float alpha = 0.1; // factor to tune
+float average = 0.5;
 
 void setup() 
 {
@@ -28,23 +32,32 @@ void setup()
 }
 
 void loop()
-{
-  int last_y = mid_y;
- 
+{ 
   uView.clear(PAGE);
   
   for (int pos_x=1; pos_x < max_x; pos_x++)
   {
+    // Audio Sample and averaging
     int value = analogRead(PIN_AUDIO_IN); 
-    float audio = (float)value/(float)AUDIO_MAX;   // 0 to 1
-    audio-=0.5;
-
-    int pos_y = mid_y+(audio*max_y);
+    float audio = normalize_audio(value);
+    average = (alpha * audio) + ((1-alpha) * average);
+    
+    // Plot
+    int pos_y = average*max_y; //mid_y+(audio*max_y);
     uView.line(pos_x-1, last_y, pos_x, pos_y);
-  
     last_y = pos_y;
   }
   
   uView.display(); 
-  delay(10);
+  delay(5);
+}
+
+float normalize_audio(int value)
+{
+   float audio = (float)value/(float)AUDIO_MAX;   // 0 to 1
+   
+   // Clamp
+   if (audio < 0.0) audio = 0.0;
+   if (audio > 1.0) audio = 1.0;
+   return audio;
 }
